eaa4f54dddcf5a812fc9fad0314851084e929e76
chaewon <djacltk@naver.com>
2023-07-27 21:59:44 +0900

    :sparkles: feat: entity 설계

---
+++ src/main/java/fast/mini/be/domain/loginlog/LoginLog.java
@@ -0,0 +1,30 @@
+package fast.mini.be.domain.loginlog;
+
+import fast.mini.be.domain.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import fast.mini.be.domain.user.User;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "login_log_tb")
+public class LoginLog extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false, length = 256)
+    private String userAgent;
+
+    @Column(nullable = false, length = 256)
+    private String clientIP;
+}
