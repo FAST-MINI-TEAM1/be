3abc91870c4dd99d77e169deb55c0bdc76b9039c
chaewon <djacltk@naver.com>
2023-07-29 22:29:47 +0900

    :sparkles: feat: 관리자 요청 관리(결제처리) Service 구현

---
+++ src/main/java/fast/mini/be/domain/admin/AdminService.java
@@ -0,0 +1,43 @@
+package fast.mini.be.domain.admin;
+
+import fast.mini.be.domain.approveDate.ApproveDate;
+import fast.mini.be.domain.approveDate.ApproveDateRepository;
+import fast.mini.be.domain.order.Order;
+import fast.mini.be.domain.order.OrderRepository;
+import fast.mini.be.domain.order.OrderStatus;
+import fast.mini.be.global.erros.exception.Exception404;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static fast.mini.be.domain.admin.AdminRequest.orderUpdateDTO;
+
+@RequiredArgsConstructor
+@Service
+public class AdminService {
+    private final OrderRepository orderRepository;
+    private final ApproveDateRepository approveDateRepository;
+
+    public void orderUpdate(orderUpdateDTO orderUpdateDTO) {
+        Order orderPS = orderRepository.findById(orderUpdateDTO.getId())
+                .orElseThrow(() -> new Exception404("결재 요청을 찾을 수 없습니다."));
+
+        OrderStatus status = orderUpdateDTO.getStatus();
+        orderPS.statusUpdate(status);
+
+        if (status == OrderStatus.APPROVE) {
+            List<LocalDateTime> dateList = orderPS.getDateList();
+
+            for (LocalDateTime date : dateList) {
+                ApproveDate approveDate = ApproveDate.builder()
+                        .order(orderPS)
+                        .user(orderPS.getUser())
+                        .date(date)
+                        .build();
+                approveDateRepository.save(approveDate);
+            }
+        }
+    }
+}
