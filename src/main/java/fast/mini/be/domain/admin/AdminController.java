2494d84fb7a1711f12870890ed0a21cc11409e54
chaewon <djacltk@naver.com>
2023-08-01 22:48:22 +0900

    :recycle: refactor: url PathVariable 유효성 검사 위치 변경
    컨트롤러 내부가 아니라 DTO 생성할 때
    생성자 내부에서 유효성 검사 후 생성

--- src/main/java/fast/mini/be/domain/admin/AdminController.java
+++ src/main/java/fast/mini/be/domain/admin/AdminController.java
@@ -18,29 +18,26 @@ public class AdminController {
     private final AdminService adminService;
 
     @PostMapping("/order/update")
-    public ResponseEntity<?> orderUpdate(@Valid @RequestBody AdminRequest.OrderUpdateDTO orderUpdateDTO){
+    public ResponseEntity<?> orderUpdate(@Valid @RequestBody AdminRequest.OrderUpdateDTO orderUpdateDTO) {
         adminService.orderUpdate(orderUpdateDTO);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/order/list/{status}")
     public ResponseEntity<?> orderListByStatus(@PathVariable("status") String status, Pageable pageable) {
-        if(!("wait".equals(status)) && !("complete".equals(status))){
-            throw new Exception400("url","잘못된 입력입니다.");
+        if (!("wait".equals(status)) && !("complete".equals(status))) {
+            throw new Exception400("url", "잘못된 입력입니다.");
         }
 
         Page<AdminResponse.OrderByStatusDTO> orderListByStatusDTO = adminService.orderListByStatus(status, pageable);
-        return new ResponseEntity<>(orderListByStatusDTO,HttpStatus.OK);
+        return new ResponseEntity<>(orderListByStatusDTO, HttpStatus.OK);
     }
 
     @GetMapping("/order/list/monthly/{orderType}")
-    public ResponseEntity<?> monthlyUserTotal(@PathVariable("orderType") String orderType, @RequestParam(value = "year")int year) {
-        if(!("duty".equals(orderType)) && !("annual".equals(orderType))){
-            throw new Exception400("url","잘못된 입력입니다.");
-        }
-
-        List<AdminResponse.MonthlyUserTotalDTO> monthlyUserTotal = adminService.monthlyUserTotal(new AdminRequest.MonthlyUserTotalDTO(orderType, year));
-        return new ResponseEntity<>(monthlyUserTotal,HttpStatus.OK);
+    public ResponseEntity<?> monthlyUserTotal(@PathVariable("orderType") String orderType, @RequestParam(value = "year") int year) {
+        AdminRequest.MonthlyUserTotalDTO requestDTO = new AdminRequest.MonthlyUserTotalDTO(orderType, year);
+        List<AdminResponse.MonthlyUserTotalDTO> monthlyUserTotal = adminService.monthlyUserTotal(requestDTO);
+        return new ResponseEntity<>(monthlyUserTotal, HttpStatus.OK);
     }
 
     @GetMapping("/order/list/{orderType}")
