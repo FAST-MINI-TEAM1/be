47a6844f1bc12d183235800cc9edd10b4eb54505
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-08-01 14:58:52 +0900

    ✨ feat: 유저 메인 페이지 조회  (#8)
    * ✨ feat: 유저 메인 페이지 조회 Service 구현
    
    * ✅ feat: 유저 메인 페이지 조회 Service Test
    
    * ✨ feat: 유저 메인 페이지 조회 Controller 구현

--- src/main/java/fast/mini/be/domain/order/OrderRepository.java
+++ src/main/java/fast/mini/be/domain/order/OrderRepository.java
@@ -1,7 +1,14 @@
 package fast.mini.be.domain.order;
 
-import fast.mini.be.domain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+
+	List<Order> findByUserEmail(String email);
+
 }
