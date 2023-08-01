008538f0e57960c7b513bc629e0ffdf0c3ca7a12
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-08-01 16:14:57 +0900

    📝fix : OrderReponse에서 LocalDateTime -> String 수정 (#11)
    * ✨ feat: 유저 메인 페이지 조회 Service 구현
    
    * ✅ feat: 유저 메인 페이지 조회 Service Test
    
    * ✨ feat: 유저 메인 페이지 조회 Controller 구현
    
    * .
    
    * 📝fix : OrderReponse에서 LocalDateTime -> String 수정

--- src/main/java/fast/mini/be/domain/order/OrderRepository.java
+++ src/main/java/fast/mini/be/domain/order/OrderRepository.java
@@ -12,4 +12,5 @@ public interface OrderRepository extends JpaRepository<Order,Long> {
     Page<Order> findAllByStatus(OrderStatus status, Pageable pageable);
 
     Page<Order> findAllByStatusNot(OrderStatus status, Pageable pageable);
+
 }
