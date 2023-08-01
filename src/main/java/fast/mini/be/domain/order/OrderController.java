72a4715abd347b023ffa70d6b76e492f1ab0a2f0
yunhyeonjin <h00j@naver.com>
2023-08-01 23:35:13 +0900

    ✨ feat: 연차,당직 등록 Controller 구현

--- src/main/java/fast/mini/be/domain/order/OrderController.java
+++ src/main/java/fast/mini/be/domain/order/OrderController.java
@@ -8,6 +8,8 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,5 +43,14 @@ public class OrderController {
 		List<OrderResponse> orders = orderService.getUserMainPage(userEmail, year, month);
 
 		return ResponseEntity.ok(orders);
+	}
+
+	@PostMapping("/order/add")
+	public ResponseEntity<String> addOrder(@Valid @RequestHeader("Authorization") String token,
+		@RequestBody OrderRequest orderRequest) {
+
+		orderService.addOrder(token, orderRequest);
+
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
