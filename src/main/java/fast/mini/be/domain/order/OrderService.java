9be69e9b7b8bee93655582821141dbfad9fdd025
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-08-01 14:58:52 +0900

    ✨ feat: 유저 메인 페이지 조회  (#8)
    * ✨ feat: 유저 메인 페이지 조회 Service 구현
    
    * ✅ feat: 유저 메인 페이지 조회 Service Test
    
    * ✨ feat: 유저 메인 페이지 조회 Controller 구현

---
+++ src/main/java/fast/mini/be/domain/order/OrderService.java
@@ -0,0 +1,49 @@
+package fast.mini.be.domain.order;
+
+
+import fast.mini.be.global.jwt.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+	private final OrderRepository orderRepository;
+	private final JwtService jwtService;
+
+	public List<OrderResponse> getUserMainPage(String token, int year, int month) {
+
+		String email = jwtService.extractUsername(token).orElseThrow(() -> new RuntimeException("유효하지 않는 토큰입니다."));
+
+		YearMonth yearMonth = YearMonth.of(year, month);
+
+		List<Order> orders = orderRepository.findByUserEmail(email);
+
+		List<OrderResponse> orderResponses = orders.stream()
+			.filter(order -> {
+				LocalDateTime startDate = order.getStartDate();
+				YearMonth orderYearMonth = YearMonth.of(startDate.getYear(), startDate.getMonth());
+				return yearMonth.equals(orderYearMonth);
+			})
+			.map(order -> new OrderResponse(
+				order.getId(),
+				order.getOrderType().getLabel(),
+				order.getStartDate(),
+				order.getEndDate(),
+				order.getStatus().getLabel()))
+			.collect(Collectors.toList());
+
+		return orderResponses;
+
+	}
+}
+
