fbf52f634ca930756bce1f52b53c1b67825c67ec
chaewon <djacltk@naver.com>
2023-08-02 16:42:42 +0900

    :sparkles: feat: 사원 연차/당직 내역 조회 service

--- src/main/java/fast/mini/be/domain/order/OrderRepository.java
+++ src/main/java/fast/mini/be/domain/order/OrderRepository.java
@@ -5,15 +5,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order,Long> {
-	List<Order> findByUserEmail(String email);
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserEmail(String email);
 
-	Page<Order> findByUserEmail(String email, Pageable pageable);
-  
+    Page<Order> findByUserEmail(String email, Pageable pageable);
+
     Page<Order> findAllByStatus(OrderStatus status, Pageable pageable);
 
     Page<Order> findAllByStatusNot(OrderStatus status, Pageable pageable);
 
+    Page<Order> findAllByUserId(long userId, Pageable pageable);
 }
