1621631ccbc095ede8c9c0746ab96b066718dc5b
chaewon <djacltk@naver.com>
2023-08-02 14:42:44 +0900

    :sparkles: feat: 사원조회 service 구현

--- src/main/java/fast/mini/be/domain/admin/AdminService.java
+++ src/main/java/fast/mini/be/domain/admin/AdminService.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -46,42 +47,44 @@ public class AdminService {
             }
         }
     }
-  
-    public Page<AdminResponse.OrderByStatusDTO> orderListByStatus(String status, Pageable pageable){
+
+    public Page<AdminResponse.OrderByStatusDTO> orderListByStatus(String status, Pageable pageable) {
         Page<Order> orderList;
-        if(OrderStatus.WAIT.name().equals(status.toUpperCase())){
-            orderList= orderRepository.findAllByStatus(OrderStatus.WAIT, pageable);
-        }else{
-            orderList= orderRepository.findAllByStatusNot(OrderStatus.WAIT, pageable);
+        if (OrderStatus.WAIT.name().equals(status.toUpperCase())) {
+            orderList = orderRepository.findAllByStatus(OrderStatus.WAIT, pageable);
+        } else {
+            orderList = orderRepository.findAllByStatusNot(OrderStatus.WAIT, pageable);
         }
 
         return AdminResponse.OrderByStatusDTO.fromEntityList(orderList);
     }
 
-    public List<AdminResponse.MonthlyUserTotalDTO> monthlyUserTotal(AdminRequest.MonthlyUserTotalDTO monthlyUserTotalDTO){
+    public List<AdminResponse.MonthlyUserTotalDTO> monthlyUserTotal(AdminRequest.MonthlyUserTotalDTO monthlyUserTotalDTO) {
         // 모든 사원에 대한 사용 대장을 구한다
         List<User> users = userRepository.findAllByRole(Role.USER)
-                .orElseThrow(()->{return new Exception404("사용자를 찾을 수 없습니다.");});
+                .orElseThrow(() -> {
+                    return new Exception404("사용자를 찾을 수 없습니다.");
+                });
 
         List<AdminResponse.MonthlyUserTotalDTO> monthlyUserTotalDTOList = new ArrayList<>();
-        for(User user : users){
+        for (User user : users) {
             // 0으로 초기화된 월별 카운트 DTO
             AdminResponse.MonthCountDTO monthCountDTO = new AdminResponse.MonthCountDTO();
 
             // 해당 사원이 승인된 요청을 가지는지 찾고 월별 카운트를 갱신한다
             Optional<List<ApproveDate>> approveDateList = approveDateRepository.findAllByUserAndOrderTypeInYear(
-                            user.getId(), monthlyUserTotalDTO.getOrderType(), monthlyUserTotalDTO.getYear()
+                    user.getId(), monthlyUserTotalDTO.getOrderType(), monthlyUserTotalDTO.getYear()
             );
             approveDateList.ifPresent(monthCountDTO::count);
 
             // 해당 사원의 사용 대장을 추가한다
-            monthlyUserTotalDTOList.add(new AdminResponse.MonthlyUserTotalDTO(user,monthCountDTO));
+            monthlyUserTotalDTOList.add(new AdminResponse.MonthlyUserTotalDTO(user, monthCountDTO));
         }
 
         return monthlyUserTotalDTOList;
     }
 
-    public List<AdminResponse.DailyOrderDTO> dailyOrderList(AdminRequest.DailyOrderListDTO dailyOrderListDTO){
+    public List<AdminResponse.DailyOrderDTO> dailyOrderList(AdminRequest.DailyOrderListDTO dailyOrderListDTO) {
         List<ApproveDate> approveDateList = approveDateRepository
                 .findAllByOrderTypeInYearMonth(
                         dailyOrderListDTO.getOrderType(), dailyOrderListDTO.getYear(), dailyOrderListDTO.getMonth()
@@ -89,5 +92,11 @@ public class AdminService {
                 .orElseThrow(() -> new Exception404("해당 데이터가 존재하지 않습니다."));
 
         return AdminResponse.DailyOrderDTO.fromEntityList(approveDateList);
+    }
+
+    public AdminResponse.UserSearchDTO userSearch(String name, String empNo) {
+        User user = userRepository.findByEmpNameAndEmpNo(name, empNo)
+                .orElseThrow(() -> new Exception404("해당유저가 존재하지 않습니다."));
+        return new AdminResponse.UserSearchDTO(user);
     }
 }
