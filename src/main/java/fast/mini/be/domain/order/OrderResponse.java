058959afee5d7d4c76f9ad219a77d652f7ffe30f
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-08-02 14:03:50 +0900

    ✨ feat: 유저 전자결재내역 조회 (#14)
    * ✨ feat: 유저 메인 페이지 조회 Service 구현
    
    * ✅ feat: 유저 메인 페이지 조회 Service Test
    
    * ✨ feat: 유저 메인 페이지 조회 Controller 구현
    
    * .
    
    * 📝fix : OrderReponse에서 LocalDateTime -> String 수정
    
    * ✨ feat: 연차,당직 등록 service 구현
    
    * ✅ feat: 연차,당직 등록 Service Test
    
    * ✨ feat: 연차,당직 등록 Controller 구현
    
    * 🎨 refactor: 단순 코드(띄어쓰기) 변경 & 연차 0 -> 12개로 수정
    
    * 🚑 fix: 테스크 코드 더미 데이터 수정
    
    * ✨ feat: 유저 전자결재내역 조회 Service 구현
    
    * ✅ feat: 유저 전자결재내역 조회 Service Test
    
    * ✨ feat: 유저 전자결재내역 조회 Controller 구현

--- src/main/java/fast/mini/be/domain/order/OrderResponse.java
+++ src/main/java/fast/mini/be/domain/order/OrderResponse.java
@@ -1,7 +1,10 @@
 package fast.mini.be.domain.order;
 
 
-import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+
+import fast.mini.be.global.utils.DateUtils;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,4 +18,32 @@ public class OrderResponse {
 	private String startDate;
 	private String endDate;
 	private String status;
+
+	@Getter
+	@Setter
+	public static class orderListByUserDto {
+		Long id;
+		String empName;
+		String createdAt;
+		String orderType;
+		String status;
+		String startDate;
+		String endDate;
+		String reason;
+
+		private orderListByUserDto(Order order){
+			this.id = order.getId();
+			this.empName = order.getUser().getEmpName();
+			this.createdAt = DateUtils.toStringFormat(order.getCreatedAt());
+			this.orderType = order.getOrderType().getLabel();
+			this.status = order.getStatus().getLabel();
+			this.startDate = DateUtils.toStringFormat(order.getStartDate());
+			this.endDate = DateUtils.toStringFormat(order.getEndDate());
+			this.reason = order.getReason();
+		}
+		public static Page<orderListByUserDto> fromOrder(Page<Order> userOrderList) {
+
+			return userOrderList.map(orderListByUserDto::new);
+		}
+	}
 }
