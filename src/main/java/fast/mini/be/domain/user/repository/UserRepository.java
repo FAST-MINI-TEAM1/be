373f286d4040eb5927a84ffa0c39a9cd29f4fdd1
chaewon <djacltk@naver.com>
2023-08-02 14:42:44 +0900

    :sparkles: feat: 사원조회 service 구현

--- src/main/java/fast/mini/be/domain/user/repository/UserRepository.java
+++ src/main/java/fast/mini/be/domain/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package fast.mini.be.domain.user.repository;
 import fast.mini.be.domain.user.Role;
 import fast.mini.be.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByRefreshToken(String refreshToken);
 
     Optional<List<User>> findAllByRole(Role role);
+
+    Optional<User> findByEmpNameAndEmpNo(@RequestParam(required = false) String empName, @RequestParam(required = false) String empNo);
 }
