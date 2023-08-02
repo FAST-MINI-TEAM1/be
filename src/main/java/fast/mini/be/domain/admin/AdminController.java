01e6ec91c8ce46a08bd2d5bbdbf19651eaf04adf
chaewon <djacltk@naver.com>
2023-08-02 17:52:11 +0900

    :sparkles: feat: controller 토큰 검사 로직 추가

--- src/main/java/fast/mini/be/domain/admin/AdminController.java
+++ src/main/java/fast/mini/be/domain/admin/AdminController.java
@@ -18,13 +18,20 @@ public class AdminController {
     private final AdminService adminService;
 
     @PostMapping("/order/update")
-    public ResponseEntity<?> orderUpdate(@Valid @RequestBody AdminRequest.OrderUpdateDTO orderUpdateDTO) {
+    public ResponseEntity<?> orderUpdate(
+            @RequestHeader("Authorization") String token,
+            @Valid @RequestBody AdminRequest.OrderUpdateDTO orderUpdateDTO
+    ) {
         adminService.orderUpdate(orderUpdateDTO);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/order/list/{status}")
-    public ResponseEntity<?> orderListByStatus(@PathVariable("status") String status, Pageable pageable) {
+    public ResponseEntity<?> orderListByStatus(
+            @RequestHeader("Authorization") String token,
+            @PathVariable("status") String status,
+            Pageable pageable
+    ) {
         if (!("wait".equals(status)) && !("complete".equals(status))) {
             throw new Exception400("url", "잘못된 입력입니다.");
         }
@@ -34,7 +41,11 @@ public class AdminController {
     }
 
     @GetMapping("/order/list/monthly/{orderType}")
-    public ResponseEntity<?> monthlyUserTotal(@PathVariable("orderType") String orderType, @RequestParam(value = "year") int year) {
+    public ResponseEntity<?> monthlyUserTotal(
+            @RequestHeader("Authorization") String token,
+            @PathVariable("orderType") String orderType,
+            @RequestParam(value = "year") int year
+    ) {
         AdminRequest.MonthlyUserTotalDTO requestDTO = new AdminRequest.MonthlyUserTotalDTO(orderType, year);
         List<AdminResponse.MonthlyUserTotalDTO> monthlyUserTotal = adminService.monthlyUserTotal(requestDTO);
         return new ResponseEntity<>(monthlyUserTotal, HttpStatus.OK);
@@ -42,6 +53,7 @@ public class AdminController {
 
     @GetMapping("/order/list/{orderType}")
     public ResponseEntity<?> dailyOrderList(
+            @RequestHeader("Authorization") String token,
             @PathVariable("orderType") String orderType,
             @RequestParam(value = "year") int year,
             @RequestParam(value = "month") int month
@@ -53,6 +65,7 @@ public class AdminController {
 
     @GetMapping("/user/search")
     public ResponseEntity<?> userSearch(
+            @RequestHeader("Authorization") String token,
             @RequestParam(value = "name", required = false) String name,
             @RequestParam(value = "empno", required = false) String empNo
     ) {
@@ -67,6 +80,7 @@ public class AdminController {
 
     @GetMapping("/order/list")
     public ResponseEntity<?> orderListByUserId(
+            @RequestHeader("Authorization") String token,
             @RequestParam(value = "user") int userId,
             Pageable pageable
     ) {
