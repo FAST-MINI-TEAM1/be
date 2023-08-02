76507f1dd77f2d492a33a18150cca0241112cf4a
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-08-02 14:03:50 +0900

    ✨ feat: 유저 전자결재내역 조회 (#14)
    * ✨ feat: 유저 메인 페이지 조회 Service 구현
    
    * ✅ feat: 유저 메인 페이지 조회 Service Test
    
    * ✨ feat: 유저 메인 페이지 조회 Controller 구현
    
    * .
    
    * 📝fix : OrderReponse에서 LocalDateTime -> String 수정
    
    * ✨ feat: 연차,당직 등록 service 구현
    
    * ✅ feat: 연차,당직 등록 Service Test
    
    * ✨ feat: 연차,당직 등록 Controller 구현
    
    * 🎨 refactor: 단순 코드(띄어쓰기) 변경 & 연차 0 -> 12개로 수정
    
    * 🚑 fix: 테스크 코드 더미 데이터 수정
    
    * ✨ feat: 유저 전자결재내역 조회 Service 구현
    
    * ✅ feat: 유저 전자결재내역 조회 Service Test
    
    * ✨ feat: 유저 전자결재내역 조회 Controller 구현

--- src/main/java/fast/mini/be/domain/order/OrderRepository.java
+++ src/main/java/fast/mini/be/domain/order/OrderRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
-    List<Order> findByUserEmail(String email);
+	List<Order> findByUserEmail(String email);
+
+	Page<Order> findByUserEmail(String email, Pageable pageable);
   
     Page<Order> findAllByStatus(OrderStatus status, Pageable pageable);
 
