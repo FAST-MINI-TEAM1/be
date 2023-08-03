33035acae55a5e9bd0363c2b5115f721a48cc8a9
ho30archive <ho30archive@gmail.com>
2023-08-03 18:32:44 +0900

    :sparkles: feat : 사원번호 추가 함수

--- src/main/java/fast/mini/be/global/utils/exception/ExMessage.java
+++ src/main/java/fast/mini/be/global/utils/exception/ExMessage.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ExMessage {
-    USER_ERROR_DB_SAVE("회원을 DB에 저장하는 중에 문제가 발생했습니다.");
+    USER_ERROR_DB_SAVE("회원을 DB에 저장하는 중에 문제가 발생했습니다."),
+    USER_ERROR_EMAIL_DUPLICATE("이메일이 중복되었습니다.");
 
     private final String message;
 }
