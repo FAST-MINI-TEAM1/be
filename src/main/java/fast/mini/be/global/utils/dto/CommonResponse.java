7591c806a19417f8bbb2068c40b280189de2d9a7
ho30archive <ho30archive@gmail.com>
2023-08-03 16:12:36 +0900

    :sparkles: feat : 결과 반환값 추가

---
+++ src/main/java/fast/mini/be/global/utils/dto/CommonResponse.java
@@ -0,0 +1,15 @@
+package fast.mini.be.global.utils.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CommonResponse {
+    SUCCESS(1, "성공"),
+    FAIL(-1, "실패");
+
+    private int code;
+    private String message;
+}
+
