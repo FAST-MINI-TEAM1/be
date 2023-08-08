66a4b4489fce2e9daf07420aa895c0965c3c60b6
ho30archive <ho30archive@gmail.com>
2023-08-09 02:57:32 +0900

    :sparkles: feat : jjwt accessToken 생성까지 구현

---
+++ src/main/java/fast/mini/be/global/jwt2/JwtTokenProvider.java
@@ -0,0 +1,86 @@
+package fast.mini.be.global.jwt2;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Base64;
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private long tokenValidTime = 1000L * 60 * 30; // 30분
+    private long refreshTokenValidTime = 1000L * 60 * 60 * 24 * 14; // 14일
+
+
+    private final UserDetailsService memberDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String email) {
+        Claims claims = Jwts.claims().setSubject(email);
+        Date now = new Date();
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(now)
+            .setExpiration(new Date(now.getTime() + tokenValidTime))
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact();
+    }
+
+    public String createRefreshToken() {
+        Date now = new Date();
+
+        return Jwts.builder()
+            .setIssuedAt(now)
+            .setExpiration(new Date(now.getTime() + refreshTokenValidTime))
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact();
+    }
+
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = memberDetailsService.loadUserByUsername(getMemberEmail(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getMemberEmail(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        } catch(ExpiredJwtException e) {
+            return e.getClaims().getSubject();
+        }
+    }
+
+    public String resolveToken(HttpServletRequest req) {
+        return req.getHeader("Authorization");
+    }
+
+    public boolean validateTokenExceptExpiration(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch(Exception e) {
+            return false;
+        }
+    }
+}
