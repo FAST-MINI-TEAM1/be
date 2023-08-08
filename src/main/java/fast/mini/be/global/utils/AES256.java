869a46814f33ee183cf69723821727d391a6eeeb
Eom Chae Won <djacltk@naver.com>
2023-08-08 17:33:07 +0900

    🚑 fix: 관리자 요청 '반려' 처리 오류 수정  (#31)
    * :ambulance: feat: 암/복호화 로그 추가
    
    * :ambulance: fix: 관리자 요청 '반려' 처리 오류 수정

--- src/main/java/fast/mini/be/global/utils/AES256.java
+++ src/main/java/fast/mini/be/global/utils/AES256.java
@@ -1,36 +1,54 @@
 package fast.mini.be.global.utils;
 
+import fast.mini.be.global.erros.exception.Exception500;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+@Slf4j
 @Component
 public class AES256 {
     public static String alg = "AES/CBC/PKCS5Padding";
     private final String key = "12345678910111213";
     private final String iv = key.substring(0, 16); // 16byte
 
-    public String encrypt(String text) throws Exception {
-        Cipher cipher = Cipher.getInstance(alg);
-        SecretKeySpec keySpec = new SecretKeySpec(iv.getBytes(), "AES");
-        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
-        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+    public String encrypt(String text) {
+        try {
+            log.info("암호화 시도  {}", text);
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
 
-        byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
-        return Base64.getEncoder().encodeToString(encrypted);
+            byte[] encrypted = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(encrypted);
+        } catch (Exception e) {
+            log.info("암호화에 실패하였습니다  {}", text);
+            e.printStackTrace();
+            throw new Exception500("서버 오류!: 암호화 실패");
+        }
     }
 
-    public String decrypt(String cipherText) throws Exception {
-        Cipher cipher = Cipher.getInstance(alg);
-        SecretKeySpec keySpec = new SecretKeySpec(iv.getBytes(), "AES");
-        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
-        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+    public String decrypt(String cipherText) {
+        try {
+            log.info("복호화 시도  {}", cipherText);
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
 
-        byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
-        byte[] decrypted = cipher.doFinal(decodedBytes);
-        return new String(decrypted, "UTF-8");
+            byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+            byte[] decrypted = cipher.doFinal(decodedBytes);
+            return new String(decrypted, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            log.info("복호화에 실패하였습니다  {}", cipherText);
+            e.printStackTrace();
+            throw new Exception500("서버 오류!: 복호화 실패");
+        }
     }
 }
